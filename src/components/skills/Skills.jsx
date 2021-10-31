9c1e803ce7546ae6187efd1b1d8fa72823c1a599
Mukhammaddiyor <mukhammaddiyor@mukhammaddiyor.local>
2021-11-01 02:32:28 +0900

    client

--- src/components/skills/Skills.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Skills() {
-	return (
-		<div className="skills__container">
-			<div className="title"></div>
-		</div>
-	);
-}
--- src/components/work/Work.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Work() {
-	return (
-		<div className="recent-work__container">
-			<div className="title"></div>
-		</div>
-	);
-}
